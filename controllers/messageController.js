bef8a64245b24be1594a7a443e3945ca099081bc
createlsworklappy <justinecastillo7@gmail.com>
2026-02-20 15:50:42 +0900

    refactor(messageController): add new message validation as hard back up

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,4 +1,12 @@
 import { getAllMessages, insertMessage } from "../db/queries.js";
+import { body, validationResult } from "express-validator";
+
+const validateMessage = [
+  body("newMessage")
+    .trim()
+    .notEmpty().withMessage("Message cannot be empty.")
+    .isLength({ max: 255 }).withMessage("Message must be 255 characters or less.")
+];
 
 async function getMessages(req, res) {
   const user = req.session.username;
@@ -15,14 +23,28 @@ async function getMessages(req, res) {
     });
 }
 
-async function createMessage(req, res) {
-  const userId = req.session.userId;
+const createMessage = [
+  validateMessage,
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  const { newMessage } = req.body;
+    if (!errors.isEmpty()) {
+      const messages = await getAllMessages();
+      const user = req.session.username;
 
-  await insertMessage({ text: newMessage, userId: userId });
+      return res.render("index", {
+        title: "Mini Message Board",
+        messages: messages,
+        user: user,
+        error: errors.array()[0].msg
+      });
+    }
 
-  res.redirect("/");
-}
+    const userId = req.session.userId;
+    const { newMessage } = req.body;
+    await insertMessage({ text: newMessage, userId: userId });
+    res.redirect("/");
+  }
+];
 
 export { getMessages, createMessage };
